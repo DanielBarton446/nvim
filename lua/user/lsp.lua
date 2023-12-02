2d303caedd8105411c08dea8909d5210406c77b5
Daniel Barton <daniel.barton446@gmail.com>
2023-12-02 04:41:10 -0600

    fix: shift-tab autocomplete missing

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -37,7 +37,8 @@ lsp.setup()
 
 
 -- need to set mapping that on Enter key, select auto completion
-local cmp = require('cmp')
+local cmp = require'cmp'
+local luasnip = require'luasnip'
 
 -- copilot tab completion
 local has_words_before = function()
@@ -49,13 +50,26 @@ end
 cmp.setup({
   mapping = {
     ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ["<Tab>"] = vim.schedule_wrap(function(fallback)
-      if cmp.visible() and has_words_before() then
-        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end),
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
   },
   sources = {
     -- copilot
