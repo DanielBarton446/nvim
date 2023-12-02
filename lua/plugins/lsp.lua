35877610b8882f66424ecc3a2b09cb0ad2d0602d
Daniel Barton <daniel.barton446@gmail.com>
2023-12-02 06:50:52 -0600

    :bug: cmp not working but lsp getting started

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,119 @@
+return {
+  -- LSP
+  "neovim/nvim-lspconfig",
+  event = "BufEnter",
+  dependencies = {
+    -- LSP
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "VonHeikemen/lsp-zero.nvim",
+
+    -- Autocompletion
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/cmp-nvim-lua' },
+
+    -- Snippets
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
+  },
+
+  config = function()
+    -- Mason for installing language servers
+    require("mason").setup()
+
+    local servers = {
+      "lua_ls",
+      "pyright",
+      "bashls",
+      "rust_analyzer",
+    }
+
+    require("mason-lspconfig").setup {
+      ensure_installed = servers,
+      automatic_installation = true,
+    }
+
+    -- LSP zero for formatting and actually attaching languag servers
+    local lsp = require('lsp-zero')
+    lsp.preset('recommended')
+
+    -- Fix Undefined global 'vim'
+    lsp.configure('lua_ls', {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { 'vim' }
+          }
+        }
+      }
+    })
+
+    lsp.set_preferences({
+      suggest_lsp_servers = true,
+      sign_icons = {
+        error = " ",
+        warn = " ",
+        hint = " ",
+        info = " "
+      }
+    })
+
+    lsp.setup()
+
+    -- CMP stuff
+
+    -- need to set mapping that on Enter key, select auto completion
+    local cmp = require'cmp'
+    local luasnip = require'luasnip'
+
+    -- copilot tab completion
+    local has_words_before = function()
+      if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_text(0, line-1, 0, line-1, col, {})[1]:match("^%s*$") == nil
+    end
+
+    cmp.setup({
+      mapping = {
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      },
+      sources = {
+        -- copilot
+        { name = "copilot"},
+        -- nvim lsp
+        { name = "nvim_lsp"},
+        { name = "path"},
+        { name = "luasnip"},
+
+        { name = "buffer" } -- use things locally if we can't get from an lsp source
+      }
+    })
+
+  end,
+
+}
