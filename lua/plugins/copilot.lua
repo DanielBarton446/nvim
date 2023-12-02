12c1eae8c9b63f53d51dcfb5aa4e31907577b47f
Daniel Barton <daniel.barton446@gmail.com>
2023-12-02 05:57:04 -0600

    :sparkles: colorscheme, copilot, and other ui things

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,27 @@
+return {
+    "zbirenbaum/copilot.lua",
+    dependencies = {
+      "zbirenbaum/copilot-cmp",
+    },
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup {
+        suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-a>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-n>",
+          prev = "<C-p>",
+          dismiss = "<C-l>",
+          },
+        },
+        copilot_node_command = "node", -- Node.js version must be > 16.x
+        server_opts_overrides = {},
+      }
+    end
+}
--- lua/user/lazyvim.lua
+++ lua/user/lazyvim.lua
@@ -11,6 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
--- Start
-require("lazy").setup(plugins, opts)
+require("lazy").setup{
+  spec = {
+    { import = "plugins" }
+  }
+}
